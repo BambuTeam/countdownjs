acd6486a99fa81f6766460dce3c2303088457a63
Luis Enrique Concuá <hola@bambu.team>
2021-06-11 14:51:59 -0600

    React-Bootstrap

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Container, Row, Col} from 'react-bootstrap';
+
+
+
+const Navbar=()=>(
+  <div className="footer">
+  <Container>
+  <Row>
+    <Col>1 of 1</Col>
+  </Row>
+</Container>
+</div>
+)
+
+export default Navbar;
